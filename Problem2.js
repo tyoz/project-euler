a399b1f3938b437fd5f83631027d720b17def7a3
tyoz <tao.zanella@gmail.com>
2018-10-25 20:42:20 -0300

    Update Problem2.js

--- Problem2.js
+++ Problem2.js
@@ -1,1 +1,21 @@
+function f () {
+	let sum = 0;
+	let n1 = 1;
+	let n2 = 1;
+	let n3 = 1;
+	
+	while(n2 <= 4000000) {
+        if(n2 % 2 === 0) {
+            sum += n2;
+        }
+        console.log(n2);
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+    }
 
+	return sum;
+}
+
+let sum = f();
+console.log(sum);
